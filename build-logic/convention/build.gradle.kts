62e4d6dde3f6443da8b55320eccd9064126dc0d6
Sergio Belda Galbis <sbeldagalbis@gmail.com>
2025-12-29 01:18:35 +0100

    Update convention build.gradle.kts

--- build-logic/convention/build.gradle.kts
+++ build-logic/convention/build.gradle.kts
@@ -19,15 +19,23 @@ dependencies {
 }
 
 gradlePlugin {
+    website.set("https://github.com/serbelga/project-config")
+    vcsUrl.set("https://github.com/serbelga/project-config")
     plugins {
         val conventionPluginsPath = "dev.sergiobelda.projectconfig.buildlogic.convention"
         create("android-library") {
             id = libs.plugins.sergiobelda.convention.android.library.get().pluginId
             implementationClass = "$conventionPluginsPath.android.AndroidLibraryConventionPlugin"
+            displayName = "android-library"
+            description = "Convention plugin for android library modules"
+            tags = listOf("convention", "android-library")
         }
         create("spotless") {
             id = libs.plugins.sergiobelda.convention.spotless.get().pluginId
             implementationClass = "$conventionPluginsPath.spotless.SpotlessConventionPlugin"
+            displayName = "spotless"
+            description = "Convention plugin that applies a custom spotless configuration"
+            tags = listOf("convention", "spotless")
         }
     }
 }
